8777f8255d0c5078bad290b2dc561867306f5a71
Nijjwal Shrestha <nijjwal.shrestha@gmail.com>
2020-06-08 20:46:24 -0600

    T1: Initial code commit for US and non-US Regions.

---
+++ corona-tracker-for-non-us-region/src/main/java/com/nijjwal/coronatrackerfornonusregion/CoronaTrackerForNonUsRegionApplication.java
@@ -0,0 +1,13 @@
+package com.nijjwal.coronatrackerfornonusregion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaTrackerForNonUsRegionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaTrackerForNonUsRegionApplication.class, args);
+	}
+
+}
---
+++ corona-tracker-for-us-region/src/main/java/com/nijjwal/coronatrackerforusregion/CoronaTrackerForUsRegionApplication.java
@@ -0,0 +1,13 @@
+package com.nijjwal.coronatrackerforusregion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaTrackerForUsRegionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaTrackerForUsRegionApplication.class, args);
+	}
+
+}
